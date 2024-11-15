2a55e5c72b45c5cbcf7c06e7eccb74c52bba58d0
Matteo Moriconi <matteo.moriconi>
2024-11-15 10:38:17 +0100

    cia0

--- programma.js
+++ programma.js
@@ -1,17 +1,36 @@
 
-
 let foglio = document.getElementById('foglio');
 
 let disegno = foglio.getContext('2d');
 
+disegno.fillStyle = 'black'
+
+disegno.fillRect(0,0, 500, 500) 
+
 disegno.fillStyle = 'red'
 
-disegno.fillRect(190,60, 80, 100) 
+disegno.fillRect(140,0, 360, 360 ) 
 
-disegno.fillStyle = 'green'
+disegno.fillStyle = 'white'
 
-disegno.fillRect(210,120, 100, 50)
+disegno.fillRect(0,0, 125, 140 ) 
 
-disegno.fillStyle = 'yellow'
+disegno.fillStyle = 'white'
 
-disegno.fillRect(190,100, 50, 100)
+disegno.fillRect(0,160, 125, 200 ) 
+
+disegno.fillStyle = 'blue'
+
+disegno.fillRect(0,375, 125, 250 ) 
+
+disegno.fillStyle = 'white'
+
+disegno.fillRect(140,370, 300, 200)
+
+
+let foglio2 = document.getElementById('foglio2');
+
+let disegno2 = foglio2.getContext('2d');
+
+foglio2.addEventListener('click', (evento) => {
+}) 
